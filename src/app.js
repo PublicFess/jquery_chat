b0563252839dd9b17d5a78a43a55560fa81f8012
Nikita Shuklin <publicfess@gmail.com>
2016-02-27 13:58:26 +0300

    completed form

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var messages = window.localStorage.getItem('messages') || [
+  global.messages = JSON.parse(window.localStorage.getItem('messages')) || [
     {
       message: 'lorem ipsum',
       children: [
@@ -14,45 +14,22 @@ $(document).ready(function() {
 
   var domMessages = $('.messages');
 
-  var addMessage = function(e) {
-    var self = this;
-    var messageObj = {
-      message: 'child',
-      children: []
-    };
-    self.children.push(messageObj);
-    var parentNode = $(e.target).parent().parent().parent().parent();
-    var message = renderMessage(messageObj);
-    parentNode.append(message);
-    message.animate({'opacity': 1}, 100);
-  };
-
-  function renderMessage(m) {
-    var message = $('<div class="message"></div>');
-    var message_content = $('<div class="message-content"></div');
-    var icon = $('<i class="material-icons">account_box</i>');
-    var message_text = $('<div class="message-text">' + m.message + '</div>');
-    var button = $('<button>Ответить</button>');
-    button.on('click', addMessage.bind(m));
-    var centered = $('<div class="centered gap"></div>');
-    centered.append(button);
-    message_text.append(centered);
-    message_content.append(icon);
-    message_content.append(message_text);
-    message.append(message_content);
-    return message;
-  };
+  var Message = require('./Message');
 
   function renderList(domNode, messages) {
     messages.forEach(function(m) {
-      var message = renderMessage(m);
-      domNode.append(message);
-      message.animate({opacity: 1}, 100);
-      if (m.children && m.children.length) {
-        renderList(message, m.children);
+      var message = new Message(m);
+      var messageDom = message.compileMessage();
+      domNode.append(messageDom);
+      messageDom.animate({opacity: 1}, 100);
+      if (message.data.children && message.data.children.length) {
+        renderList(messageDom, message.data.children);
       }
     });
   };
-  renderList(domMessages, messages);
+  renderList(domMessages, global.messages);
 
+  $('#close_form').on('click', function() {
+    $('#form').hide();
+  });
 });

78bc392626e1d66c34e3e0ed54796091ae92321d
Nikita Shuklin <publicfess@gmail.com>
2016-02-27 11:32:02 +0300

    sketched view

--- src/app.js
+++ src/app.js
@@ -21,26 +21,33 @@ $(document).ready(function() {
       children: []
     };
     self.children.push(messageObj);
-    var parentNode = $(e.target).parent();
-    var message = $('<div class="message">' +
-    '<span>' + messageObj.message + '</span>' +
-    '</div>');
-    var button = $('<button>Ответить</button>');
-    button.on('click', addMessage.bind(messageObj));
-    message.append(button);
+    var parentNode = $(e.target).parent().parent().parent().parent();
+    var message = renderMessage(messageObj);
     parentNode.append(message);
+    message.animate({'opacity': 1}, 100);
+  };
 
+  function renderMessage(m) {
+    var message = $('<div class="message"></div>');
+    var message_content = $('<div class="message-content"></div');
+    var icon = $('<i class="material-icons">account_box</i>');
+    var message_text = $('<div class="message-text">' + m.message + '</div>');
+    var button = $('<button>Ответить</button>');
+    button.on('click', addMessage.bind(m));
+    var centered = $('<div class="centered gap"></div>');
+    centered.append(button);
+    message_text.append(centered);
+    message_content.append(icon);
+    message_content.append(message_text);
+    message.append(message_content);
+    return message;
   };
 
   function renderList(domNode, messages) {
     messages.forEach(function(m) {
-      var message = $('<div class="message">' +
-      '<span>' + m.message + '</span>' +
-      '</div>');
-      var button = $('<button>Ответить</button>');
-      button.on('click', addMessage.bind(m));
-      message.append(button);
+      var message = renderMessage(m);
       domNode.append(message);
+      message.animate({opacity: 1}, 100);
       if (m.children && m.children.length) {
         renderList(message, m.children);
       }
